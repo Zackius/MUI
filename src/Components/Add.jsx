ce5d67f4bb4248678c97e4871d5ff42e9a29323c
Zacharia <ndunguzachary24@gmail.com>
2023-05-26 23:59:08 +0300

    Add button

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -1,16 +1,14 @@
 import { Fab, Tooltip } from '@mui/material'
-import React from 'react'
 import {Add as AddIcon } from "@mui/icons-material"
 
 const Add = () => {
   return (
       <>
-          <Tooltip  title="Delete" sx={{position: "fixed",  bottom: "20",  left: {xs: "calc(50% -25px)", md:30} }}>
-          <Fab color="error" aria-label="add">
+          <Tooltip  title="Add" sx={{positon: "fixed", bottom: "20", left: {xs: "calc(50% - 25px)"} , md:30}}>
+          <Fab color="primary" aria-label="add">
   <AddIcon />
 </Fab>
           </Tooltip>
-          
     </>
   )
 }
